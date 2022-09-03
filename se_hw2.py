6e3d2041746d2dbabeba71f75aea36753426da2f
Shivesh Jha <54213621+ShiveshJha12@users.noreply.github.com>
2022-09-03 11:26:43 -0400

    Contains nums class

---
+++ se_hw2.py
@@ -0,0 +1,39 @@
+class Nums():
+    def __init__(self, cpos, cname):
+        self.n = 0
+        self.at = cpos
+        self.name = cname
+        self. lo = MAXINT
+        self. hi = -MAXINT
+        self.isSorted = True
+
+    def nums(self):
+        if not self.isSorted:
+            self._has = sorted(self._has.items(), key=lambda x: x[1])
+            self.isSorted = True
+        return self._has()
+
+    def add(self, x, pos):
+        if x != '?':
+            self.n = self.n+1
+            self.lo = min(x, self.lo)
+            self.hi = max(x, self.hi)
+        ## to be written ahead
+
+    def div(self):
+        a = self.nums()
+        a90 = a[int(math.ceil((a.len() * 90) / 100)) - 1]
+        a10 = a[int(math.ceil((a.len() * 10) / 100)) - 1]
+        return ((a90-a10/2.58))
+
+
+    def mid(self):
+        a = self.nums()
+        len = a.len()
+        return (a[len//2])
+
+
+
+        
+
+    
